142684bdab6be821f511e8cc738ede1006beb2fd
Steffen Ritter <steffen.ritter@aoe.com>
2018-06-22 09:52:24 +0200

    Add scripts

--- install.sh
+++ install.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
-apt install gphoto2 imagemagick
+
+IMAGEPATH=/home/pi/pictures
+
+
+apt install gphoto2
+
+cp -f shell/postProcessCameraDownload /usr/local/bin
+chmod a+x /usr/local/bin/postProcessCameraDownload
+
+touch ${IMAGEPATH}/currentImage.txt
+
+mkdir -p /home/pi/.config/PhotoBooth
+echo <<<EOF > /home/pi/.config/PhotoBooth/config.json
+{
+    "imagePath": "${IMAGEPATH}"
+}
+EOF
+
+cp systemd/gphotodownloader.service /etc/systemd/system/
+systemctl reload
+systemctl enable gphotodownloader
+systemctl start gphotodownloader
